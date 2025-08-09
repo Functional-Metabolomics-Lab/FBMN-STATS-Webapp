9bb0e58d261975adce44bc4afca934bbb703ac9d
rithi-krishnaraj <rithi1203@gmail.com>
2025-08-09 19:59:10 +0530

    Fixed Page 2

--- pages/3_PERMANOVA_&_PCoA.py
+++ pages/3_PERMANOVA_&_PCoA.py
@@ -24,57 +24,81 @@ try:
 
     if not st.session_state.data.empty:
         c1, c2 = st.columns(2)
-        c1.selectbox(
-            "attribute for multivariate analysis",
-            [c for c in st.session_state.md.columns if len(set(st.session_state.md[c])) > 1 and len(set(st.session_state.md[c])) != st.session_state.md.shape[0]],
-            key="pcoa_attribute",
-        )
-        c2.selectbox(
-            "distance matrix",
-            [
-                "braycurtis",
-                "canberra",
-                "chebyshev",
-                "cityblock",
-                "correlation",
-                "cosine",
-                "euclidean",
-                "hamming",
-                "jaccard",
-                "matching",
-                "minkowski",
-                "seuclidean",
-                "sqeuclidean"
-            ],
-            key="pcoa_distance_matrix",
-        )
-        
-        permanova, pcoa_result = permanova_pcoa(
-            st.session_state.data,
-            st.session_state.pcoa_distance_matrix,
-            st.session_state.md[st.session_state.pcoa_attribute],
-        )
-
-        if not permanova.empty:
-            t1, t2, t3 = st.tabs(
-                [
-                    "📁 PERMANOVA statistics",
-                    "📈 Principal Coordinate Analysis",
-                    "📊 Explained variance",
-                ]
+        with c1: 
+            st.selectbox(
+                "attribute for multivariate analysis",
+                [c for c in st.session_state.md.columns if len(set(st.session_state.md[c])) > 1 and len(set(st.session_state.md[c])) != st.session_state.md.shape[0]],
+                key="pcoa_attribute",
             )
-            with t1:
-                show_table(permanova, "PERMANOVA-statistics")
-            with t2:
-                fig = get_pcoa_scatter_plot(
-                    pcoa_result,
-                    st.session_state.md,
-                    st.session_state.pcoa_attribute,
-                )
-                show_fig(fig, "principal-coordinate-analysis")
-            with t3:
-                fig = get_pcoa_variance_plot(pcoa_result)
-                show_fig(fig, "pcoa-variance")
+        with c2: 
+            st.selectbox(
+                "distance matrix",
+                ["braycurtis", "canberra", "chebyshev", "cityblock", "correlation", "cosine", "euclidean", "hamming", "jaccard", "matching", "minkowski", "seuclidean", "sqeuclidean"],
+                key="pcoa_distance_matrix",
+                index = 6
+            )
+
+        max_allowed = min(st.session_state.data.shape[0], st.session_state.data.shape[1])
+        n_component = min(10, max_allowed)
+        st.session_state["n_components"] = n_component
+
+        attribute_series = st.session_state.md[st.session_state.pcoa_attribute].dropna()
+        unique_categories = attribute_series.nunique()
+
+        max_pcoa = 10
+        pcoa_labels = [f"PC{i+1}" for i in range(max_pcoa)]
+
+        col1, col2 = st.columns(2)
+        with col1:
+            pcoa_x_axis = st.selectbox("Interested X-axis for plot", pcoa_labels)
+        with col2:
+            pcoa_y_axis = st.selectbox("Interested Y-axis for plot", pcoa_labels, index = 1)
+
+        att_col = st.session_state.pcoa_attribute
+        
+        options = sorted(st.session_state.md[att_col].dropna().unique())
+        allowed_categories = st.multiselect(
+            f"Filter categories in '{att_col}'", 
+            options = options,
+            default = options, 
+            key = "pcoa_category_filter"
+        )
+        selected_categories = st.session_state.pcoa_category_filter
+        filtered_md = st.session_state.md[st.session_state.md[att_col].isin(selected_categories)]
+        filtered_data = st.session_state.data.loc[filtered_md.index]
+
+
+        if len(filtered_md[att_col].unique()) < 2:
+            st.warning("⚠️ PERMANOVA cannot be calculated for this group because there is only one category in the selected attribute. You need at least two categories to perform statistical testing.")
+        else:
+            permanova, pcoa_result = permanova_pcoa(
+                filtered_data,
+                st.session_state.pcoa_distance_matrix,
+                st.session_state.md[st.session_state.pcoa_attribute],
+            )
+
+            if pcoa_x_axis == pcoa_y_axis:
+                st.warning("⚠️ X-axis and Y-axis cannot be the same. Please choose different axes to view results.")
+            else: 
+                if not permanova.empty:
+                    t1, t2, t3, t4 = st.tabs(["📁 PERMANOVA statistics", "📈 Principal Coordinate Analysis", "📊 Explained variance", "📁 Data"])
+                    with t1:
+                        show_table(permanova, "PERMANOVA-statistics")
+                    with t2:
+                        fig = get_pcoa_scatter_plot(
+                            pcoa_result,
+                            filtered_md,
+                            st.session_state.pcoa_attribute,
+                            pcoa_x_axis,
+                            pcoa_y_axis, 
+                            st.session_state.pcoa_category_filter
+                        )
+                        show_fig(fig, "principal-coordinate-analysis")
+                    with t3:
+                        fig = get_pcoa_variance_plot(pcoa_result)
+                        show_fig(fig, "pcoa-variance")
+                    with t4:
+                        show_table(pcoa_result.samples.iloc[:, :10], "principal-coordinates")
 
     else:
         st.warning("Please complete data preparation step first!")
